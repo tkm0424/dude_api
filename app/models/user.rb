6583465d7c50a49f271c30bc4e8a1ee5f282418f
tkm0424 <fcp.t4ku@gmail.com>
2022-01-12 22:30:36 +0900

    Add email custom validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,16 @@
+require "validator/email_validator"
+
 class User < ApplicationRecord
+  before_validation :downcase_email
+
   has_secure_password
 
   # validates
   validates :name, presence: true,
   length: { maximum: 30, allow_blank: true }
 
-#  validates :email, presence: true,
-#  email: { allow_blank: true }
+  validates :email, presence: true,
+  email: { allow_blank: true }
 
   VALID_PASSWORD_REGEX = /\A[\w\-]+\z/
   validates :password, presence: true,
@@ -16,4 +20,26 @@ class User < ApplicationRecord
     message: :invalid_password
   },
   allow_nil: true
+
+  ## methods
+  # class method 
+  class << self
+    # emailからアクティブなユーザーを返す
+    def find_by_activated(email)
+      find_by(email: email, activated: true)
+    end
+  end
+  # class method endo
+
+  # 自分以外の同じemailのアクティブなユーザーがいる場合にtrueを返す
+  def email_activated?
+    users = User.where.not(id: id)
+    users.find_by_activated(email).present?
+  end
+
+  private
+  # email小文字化
+  def downcase_email
+    self.email.downcase! if email
+  end
 end
