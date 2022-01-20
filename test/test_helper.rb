c04379e1e2f38612d02adc7d4820965ad666c470
tkm0424 <fcp.t4ku@gmail.com>
2022-01-20 22:03:36 +0900

    setup users model & test setup

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,12 +2,46 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
+# gem 'minitest-reporters' setup
+require "minitest/reporters"
+Minitest::Reporters.use!
+
 class ActiveSupport::TestCase
+  # プロセスが分岐した直後に呼び出し
+  parallelize_setup do |worker|
+    load "#{Rails.root}/db/seeds.rb"
+  end
+
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
 
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+  def active_user
+    User.find_by(activated: true)
+  end
+  
+  # api path
+  def api(path = "/")
+    "/api/v1#{path}"
+  end
 
-  # Add more helper methods to be used by all tests here...
+  # 認可ヘッダ
+  def auth(token)
+    { Authorization: "Bearer #{token}" }
+  end
+
+  # 引数のparamsでログインを行う
+  def login(params)
+    post api("/auth_token"), xhr: true, params: params
+  end
+
+  # ログアウトapi
+  def logout
+    delete api("/auth_token"), xhr: true
+  end
+
+  # レスポンスJSONをハッシュで返す
+  def res_body
+    JSON.parse(@response.body)
+  end
+
 end
